c5a31ff49d2e1b6c12969fa00470fe65001f5cbc
DataPartnerBr <datapartner.developer@gmail.com>
2018-06-02 22:07:13 -0300

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,43 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { DPApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { IntroPage } from '../pages/intro/intro';
+import { RegisterPage } from '../pages/register/register';
 import { TabsPage } from '../pages/tabs/tabs';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
+    DPApp,
     AboutPage,
     ContactPage,
     HomePage,
+    IntroPage,
+    RegisterPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    //RegisterPageModule,
+    //IntroPageModule,
+    IonicModule.forRoot(DPApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    DPApp,
     AboutPage,
     ContactPage,
     HomePage,
+    IntroPage,
+    RegisterPage,
     TabsPage
   ],
   providers: [
